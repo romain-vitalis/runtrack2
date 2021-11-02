9f3bbd1ecc9d984b0f5c3f83c1e462023016dd88
romain-vitalis <romain.vitalis@laplateforme.iO>
2021-11-02 10:25:54 +0100

    jour 1

---
+++ jour01/job01/index.php
@@ -0,0 +1,23 @@
+<?php
+$str ="LaPlateforme";
+$str2 ="Vive";
+$str3 ="!";
+echo "$str2 $str $str3";
+?>
+<br>
+<?php
+$val=6;
+$val = $val +4;
+echo $val;
+?>
+<br>
+
+
+
+<?php
+$myBool= true;
+    echo "$myBool" . '<br>';
+
+$myBool =false;
+    echo "$myBool" . '  <br>';
+?>
---
+++ jour01/job02/index.php
@@ -0,0 +1,8 @@
+<?php
+echo 'Hello LaPlateforme!','<br>';
+//commentaire 1//
+
+echo 'Hello LaPlateforme!';
+//*commentaire 2//
+
+?>
