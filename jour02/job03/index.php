0f307d26c4657595b64260abd707224aa1806555
romain-vitalis <romain.vitalis@laplateforme.iO>
2021-11-03 19:17:55 +0100

    jour 2

--- jour02/job03/index.php
+++ jour02/job03/index.php
@@ -6,13 +6,14 @@ while ($i <=100 ) {
     if ($i >= 0 && $i <=20) {
     echo"<i>",$i,"</i><br>";
     }
+    
+    elseif ($i >= 25 && $i <=50) {
+        echo"<u>",$i,"</u><br>"; 
+    }
     elseif ($i==42){
         echo"LaPlateforme_","<br>";
         
  }
-    elseif ($i >= 25 && $i <=50) {
-        echo"<u>",$i,"</u><br>"; 
-    }
     
     
 else {echo $i,"<br>";}
