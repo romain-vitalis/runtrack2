8d5da07c4fd86023add1205f86dc1f5c76a4f47a
romain-vitalis <romain.vitalis@laplateforme.iO>
2021-11-04 17:29:18 +0100

    jour3

---
+++ jour03/job04/index.php
@@ -0,0 +1,6 @@
+<?php
+$str="Dans l'espace, personne ne vous entend crier";
+for($i=0; isset($str[$i]) == TRUE ;$i++);
+
+echo $i;
+?>
---
+++ jour03/job06/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$str="Les choses que l'on possede finissent par nous posseder";
+
+for($i=0; isset($str[$i]) == TRUE ;$i++);
+
+
+for($i=$i-1; isset($str[$i]) == TRUE ;$i--){
+    echo($str[$i]);
+
+}
+
+
+?>
