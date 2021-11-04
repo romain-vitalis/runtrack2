bb77f4a3d6a770ac6ea090b59a7c38631783694c
romain-vitalis <romain.vitalis@laplateforme.iO>
2021-11-04 15:45:57 +0100

    jour3

---
+++ jour03/job01/index.php
@@ -0,0 +1,13 @@
+<?php
+$tableau= array(98,171,173,200,204,404,459);
+foreach($tableau as $nombre){
+if ($nombre % 2 == 0){
+echo $nombre ," est un nombre pair","<br>";
+}
+else {
+    echo $nombre ," est un nombre impair","<br>";
+    }
+
+}
+?>
+
