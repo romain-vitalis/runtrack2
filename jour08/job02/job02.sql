9da7aeaa8a90495da3e8b1b96a6abc73450fc60b
romain-vitalis <romain.vitalis@laplateforme.iO>
2021-11-10 16:39:45 +0100

    jour 8

---
+++ jour08/job02/job02.sql
@@ -0,0 +1,22 @@
+INSERT INTO etudiants(prenom, nom, naissance, sexe, email)
+VALUES
+('Cyril', 'Zimmermann', '1989-01-02', 'Homme', 'cyril@laplateforme.io'),
+('Jessica', 'Soriano', '1995-09-08', 'Femme', 'jessica@laplateforme.io'),
+('Roxan', 'Roumégas', '2016-09-08', 'Homme', 'roxan@laplateforme.io'),
+('Pascal', 'Assens', '1999-12-31', 'Homme', 'pascal@laplateforme.io'),
+('Terry', 'Cristinelli', '2005-02-01', 'Homme','terry@laplateforme.io'),
+('Ruben', 'Habib', '1993-05-26', 'Homme', 'ruben.habib@laplateforme.io'),
+('Toto', 'Dupont', '2019-11-07', 'Homme', 'toto@laplateforme.io')
+
+INSERT INTO `etage` (`id`, `nom`, `numero`, `superficie`) VALUES
+(1, 'RDC', 0, 500),
+(2, 'R+1', 1, 500);
+
+INSERT INTO salles (nom, id_etage, capacite)
+ VALUES
+ ('Lounge', '1', '100'),
+ ('Studio Son', '1', '5'),
+ ('Broadcasting', '2', '50'),
+ ('Bocal Peda', '2', '4'),
+ ('Coworking', '2', '80'),
+ ('Studio Video', '2', '5')
